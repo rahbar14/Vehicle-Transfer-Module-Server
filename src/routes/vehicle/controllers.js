202aa2b5a9e9362f42030c3668704656fbb1d0b2
rahbar12 <rahbarkhan97@yahoo.in>
2024-08-21 19:33:06 +0530

    create vehicle duplicate fix"

--- src/routes/vehicle/controllers.js
+++ src/routes/vehicle/controllers.js
@@ -2,30 +2,41 @@ const { ENTITY_TYPES } = require("../../config/config");
 const { mysqlPromise } = require("../../connection/db");
 
 exports.createVehicle = async (req) => {
-    const { vehicle_number, vehicle_type } = req.body;
-    const puc_certificate = req.files.puc_certificate[0]
-    const insurance_cert = req.files.insurance_cert[0]
-    const puc_path = `/uploads/${puc_certificate.folder}/${puc_certificate.filename}`
-    const inc_path = `/uploads/${insurance_cert.folder}/${insurance_cert.filename}`
-
-
-    const conn = await mysqlPromise.getConnection();
-    req.conn = conn;
-    await conn.beginTransaction();
-
-    const [driver] = await conn.query(
-        `INSERT INTO vehicles (vehicle_number, vehicle_type, puc_certificate, insurance_cert) VALUES ?`,
-        [[[vehicle_number, vehicle_type, puc_path, inc_path]]]
-    )
-
-    await conn.commit()
-    conn.release();
-
-    return {
-        status: true,
-        message: "Driver created successfully",
-        data: driver.insertId
+    try {
+        const { vehicle_number, vehicle_type } = req.body;
+        const puc_certificate = req.files.puc_certificate[0]
+        const insurance_cert = req.files.insurance_cert[0]
+        const puc_path = `/uploads/${puc_certificate.folder}/${puc_certificate.filename}`
+        const inc_path = `/uploads/${insurance_cert.folder}/${insurance_cert.filename}`
+    
+    
+        const conn = await mysqlPromise.getConnection();
+        req.conn = conn;
+        await conn.beginTransaction();
+    
+        const [driver] = await conn.query(
+            `INSERT INTO vehicles (vehicle_number, vehicle_type, puc_certificate, insurance_cert) VALUES ?`,
+            [[[vehicle_number, vehicle_type, puc_path, inc_path]]]
+        )
+    
+        await conn.commit()
+        conn.release();
+    
+        return {
+            status: true,
+            message: "Driver created successfully",
+            data: driver.insertId
+        }
+    } catch (error) {
+    if (error.code === "ER_DUP_ENTRY") {
+        return {
+            statusCode: 400,
+            message: "Vehicle already registered",
+            data: []
+        }
     }
+    throw error;
+}
 
 }
 
